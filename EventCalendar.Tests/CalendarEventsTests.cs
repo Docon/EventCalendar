5d7bf8183f46c34c0c543b1abc03f736f6a65996
Docon <diane.oconnell@velir.com>
2014-09-01 13:01:43 -0400

    Adding unit test framework

---
+++ EventCalendar.Tests/CalendarEventsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventCalendar.Data.Models;
+using NUnit.Framework;
+
+namespace EventCalendar.Tests
+{
+    [TestFixture]
+    public class CalendarEventsTests
+    {
+        [Test]
+        public void CalendarEvents_GetCalendarDayEvents_ReturnsEventsForDay()
+        {
+            List<ICalendarDayEvent> events = new List<ICalendarDayEvent>();
+        }
+    }
+}
